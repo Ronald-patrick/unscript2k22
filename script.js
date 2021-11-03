61bd2ffad7afc9968abef4e46f5b64769154d258
Ronald <ronald.patrick1101@gmail.com>
2021-11-03 11:07:39 +0530

    mobile nav added

--- script.js
+++ script.js
@@ -3,8 +3,23 @@ $(document).ready(function() {
 		console.log($(document).scrollTop());
 	  if ($(document).scrollTop() > 200) {
 		$(".main-head").addClass("head-bg");
+		$(".mob-head").addClass("head-bg");
+
+		
 	  } else {
 		$(".main-head").removeClass("head-bg");
+		$(".mob-head").removeClass("head-bg");
 	  }
 	});
   });
+
+const navbtn = document.getElementById('nav-btn');
+const navmenu = document.getElementsByClassName('nav-menu')[0];
+
+navbtn.addEventListener('click',()=>{
+	if (navmenu.style.display === "none") {
+		navmenu.style.display = "flex";
+	  } else {
+		navmenu.style.display = "none";
+	  }
+})
